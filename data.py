454b78262747a8a3c46e22d9642d9a84ffe95861
Anna Dunster <llama747@gmail.com>
2021-02-21 17:18:31 -0800

    Comment and refine code

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ import math
 import sqlite3
 import time
 
-# Allow code testing on non i2c systems:
+# Allow other code to be tested on non i2c enabled systems
 try:
     import adafruit_bme280
     import board
@@ -14,7 +14,7 @@ except NotImplementedError:
 
 class FakeBME:
     """
-    Used for testing various code on systems without a bme280 sensor
+    Used for testing non-sensor related code on systems without a bme280 sensor.
     """
 
     temperature = 30.0
@@ -27,6 +27,8 @@ def connections_setup():
     global bme280
     global dbconn
     global dbcursor
+
+    # If sensor connection cannot be loaded correctly, uses the dummy item instead.
     try:
         i2c = busio.I2C(board.SCL, board.SDA)
         bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
@@ -38,11 +40,16 @@ def connections_setup():
         i2c = None
         bme280 = FakeBME()
     finally:
+        # Set up sqlite connection regardless.
         dbconn = sqlite3.connect("readings.db")
         dbcursor = dbconn.cursor()
 
 
 def table_setup():
+    """
+    Create the table to store readings. Report error if any (typically reports
+    table already exists.)
+    """
     create_table = (
         "CREATE TABLE readings (temperature REAL, humidity REAL, "
         "pressure REAL, dewpoint REAL, date INTEGER, time INTEGER)"
@@ -55,6 +62,10 @@ def table_setup():
 
 
 def get_data():
+    """
+    Retrieves data from sensor, calculates conversion to Fahrenheit, calculates
+    current dew point, returns data.
+    """
     temp_f = (bme280.temperature * 1.8) + 32
     humidity = bme280.humidity
     pressure = bme280.pressure
@@ -83,6 +94,9 @@ def get_datetime():
 
 
 def write_data(temp_f, humidity, pressure, dewpoint, dtoutput):
+    """
+    Writes the data to the sqlite table.
+    """
     insert_command = "INSERT INTO readings VALUES (?, ?, ?, ?, ?, ?)"
 
     dbcursor.execute(
@@ -90,10 +104,9 @@ def write_data(temp_f, humidity, pressure, dewpoint, dtoutput):
         [temp_f, humidity, pressure, dewpoint, dtoutput[0], dtoutput[1]],
     )
 
+    # Commit changes in function in case of unexpected program exit.
     dbconn.commit()
-    time = datetime.datetime.strptime(
-        (dtoutput[0] + dtoutput[1]), "%Y%m%d%H%M%S"
-        )
+    time = datetime.datetime.strptime((dtoutput[0] + dtoutput[1]), "%Y%m%d%H%M%S")
     print(
         f"Saved at: {time}: "
         f"Temp: {temp_f}, Humidity: {humidity}, Pressure: {pressure}"
@@ -101,15 +114,32 @@ def write_data(temp_f, humidity, pressure, dewpoint, dtoutput):
 
 
 def read_db():
+    """
+    Gets all data from sqlite table.
+    """
     dbcursor.execute("SELECT * FROM readings")
     results = dbcursor.fetchall()
     return results
 
 
+def read_db_one():
+    """
+    Gets most recent line from sqlite table.
+    """
+    dbcursor.execute("SELECT * FROM readings ORDER BY date DESC, time DESC")
+    results = dbcursor.fetchone()
+    return results
+
+
 if __name__ == "__main__":
+    # Attempt to set up connections and create the table.
     connections_setup()
     table_setup()
 
+    # Get data once without recording due to sensor anomolies
+    get_data()
+
+    # Then start the sensor recording loop.
     while True:
         try:
             write_data(*get_data(), get_datetime())
@@ -118,10 +148,7 @@ if __name__ == "__main__":
             print("Exiting...")
             break
 
-    print(read_db())
-
-    print(dbconn.total_changes)
-
+    # Save any lingering changes and close database connections before exit.
     dbconn.commit()
     dbcursor.close()
     dbconn.close()
